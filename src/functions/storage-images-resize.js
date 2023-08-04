518282b25a4758517092d02a0d8e3ff989b533ed
구영우 <gou1226@marketboro.com>
2023-08-04 09:38:19 +0900

    PLATDEV-1476 [추가] Status Code가 200이 아닐 때 404 페이지로 redirect

--- src/functions/storage-images-resize.js
+++ src/functions/storage-images-resize.js
@@ -10,6 +10,22 @@ exports.handler = async (event, context, callback) => {
     let queryParams;
     let objectKey;
 
+    const foodspringNotFoundPageUrl = 'https://globalcf.dev.marketboro.com/error/foodspring/404.html';
+    const marketbomNotFoundPageUrl = 'https://globalcf.dev.marketboro.com/error/marketbom/404.html';
+
+    // 정상이 아닐때 모두 not found로 이동하도록 처리
+    if (response.status !== '200') {
+        response.status = '302';
+        if (response.uri && request.uri.split('/')[1] === 'foodspring') {
+            response.headers.location = [{key: 'Location', value: foodspringNotFoundPageUrl}]
+        } else {
+            response.headers.location = [{key: 'Location', value: marketbomNotFoundPageUrl}];
+        }
+        callback(null, response);
+        return
+    }
+
+
     try {
         // Parameters are w, h, q, webp and indicate width, height, quality and webp convert.
         queryParams = querystring.parse(request.querystring);
