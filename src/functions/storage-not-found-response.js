fa22e8da515b26e24ad309466b6a066a7a2dcc4c
구영우 <gou1226@marketboro.com>
2023-08-11 15:51:35 +0900

    PLATDEV-1491 [수정] console.log 제거

--- src/functions/storage-not-found-response.js
+++ src/functions/storage-not-found-response.js
@@ -3,9 +3,6 @@ const { redirectToNotFound, isStatusNotFound } = require('../utils/redirect-to-n
 exports.handler = async (event, context, callback) => {
     const { request, response } = event.Records[0].cf;
 
-    console.log(request);
-    console.log(response);
-
     // 정상이 아닐때 모두 not found로 이동하도록 처리
     if (isStatusNotFound(response)) return redirectToNotFound(request, response, callback);
 
